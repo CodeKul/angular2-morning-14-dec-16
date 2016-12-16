030eeda037596a90f5ddf6e2514c47363998210a
aniruddha <aniruddha.kudalkar@gmail.com>
2016-12-16 13:11:03 +0530

    data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,22 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 import { FlatcompComponent } from './my-first-comp/flatcomp.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { PropertybindingComponent } from './databinding/propertybinding.component';
+import { EventbindingComponent } from './databinding/eventbinding.component';
+import { TwowaybindingComponent } from './databinding/twowaybinding.component';
+import { DemoexampleComponent } from './databinding/demoexample.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MyFirstCompComponent,
-    FlatcompComponent
+    FlatcompComponent,
+    DatabindingComponent,
+    PropertybindingComponent,
+    EventbindingComponent,
+    TwowaybindingComponent,
+    DemoexampleComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/databinding/eventbinding.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventbinding',
+  templateUrl: './eventbinding.component.html',
+  styleUrls: ['./eventbinding.component.css']
+})
+export class EventbindingComponent  {
+
+  constructor() { }
+
+  public onEvenBindingClicked() {  
+    alert('yes u caught it');
+  }
+}
